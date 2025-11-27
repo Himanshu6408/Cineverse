bb03f2d6c849b1d69cbe713f643910f7e1f183cf
Himanshu Mishra <your-email@example.com>
2025-11-27 15:28:40 +0530

    first

--- src/Components/DetailsPage.jsx
+++ src/Components/DetailsPage.jsx
@@ -61,7 +61,7 @@ function DetailsPage() {
                 Director :<p>{data.Director}</p>
               </span>
               <span className="flex gap-x-3 mt-1 ">
-                Writerss :<p>{data.Writer}</p>
+                Writers :<p>{data.Writer}</p>
               </span>
               <span className="flex gap-x-3 mt-1 ">
                 Language :<p>{data.Language}</p>
